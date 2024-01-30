5c775d45e76572b050d8b251c42d0d5cf5420528
hamza <hamzahannache98@gmail.com>
2024-01-30 01:28:32 -0500

    Ajout de temps de traitement

--- src/main/java/commy/tennis/Main.java
+++ src/main/java/commy/tennis/Main.java
@@ -10,6 +10,14 @@ public class Main {
         System.out.println("Donner une valeur"); // Demande à l'utilisateur de saisir une valeur
         long nombre = sc.nextLong();
         Fibonacci F = new Fibonacci(); // Création d'un objet Fibonacci
-        System.out.println(F.fibonacciboucle(nombre)); // Affichage du résultat du calcul de la suite de Fibonacci
+
+        //Calcul du temps de traitement
+        long startTime = System.nanoTime();
+        long resultClassique = F.fibonacciboucle(nombre); //Calcul de la suite Fibonacci
+        long endTime = System.nanoTime();
+        long duree = (endTime - startTime) / 1000; // en microsecondes
+        // Affichage du résultat et du temps de traitement du calcul de la suite de Fibonacci
+        System.out.println("Suite fibonacci est : "+resultClassique);
+        System.out.println("La durée du traitement est :"+duree+" microsecondes ");
     }
 }
